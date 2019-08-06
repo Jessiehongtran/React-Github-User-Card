bd82f510eee75195d87ae79880e9492ff6533ffa
Your Name <you@example.com>
2019-08-06 17:17:44 -0400

    fetched user data

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -4,15 +4,41 @@ import './App.css';
 class App extends React.Component {
   constructor (){
       super();
-
+      this.state = {
+        name: 'Hong',
+        users: []
+      }
 
 
   }
 
+  fetchUsers = () => {
+    fetch(`https://api.github.com/users/jessiehongtran`)
+    .then(
+      response => {
+        return response.json();
+      }
+    )
+    .then(
+      getUser => console.log('getUser', getUser)
+    )
+    .catch(
+      err => console.log('err', err)
+    )
+  }
+
 
   render(){
+      this.fetchUsers();
       return (
-          <div>Welcome to GitHub UserCard App!</div>
+          <div>
+            <h1>Welcome to GitHub UserCard App created by {this.state.name}!</h1>
+            {this.state.users.map( user => {
+              return <div>'user'</div>
+            }
+            )}
+            
+          </div>
       )
   }
 }
