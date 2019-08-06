81bed5ed59f4ff908247551b9a78fd1f334ab251
Your Name <you@example.com>
2019-08-06 17:06:24 -0400

    Setup App and created App class

---
+++ github-usercard/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor (){
+      super();
+
+
+
+  }
+
+
+  render(){
+      return (
+          <div>Welcome to GitHub UserCard App!</div>
+      )
+  }
+}
+
+export default App;
